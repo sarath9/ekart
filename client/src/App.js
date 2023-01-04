4d03f6ed8d64076e0d7a8429835db8b21aec42e8
= <sarath9188@gmail.com>
2023-01-04 16:53:29 +0530

    added api

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
   <h1>Ekart</h1>
+  <Register />
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("server file")
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+app.use(cors())
+app.use(express.json())
+app.get('/test', (req,res) => {
+    res.send('Hello world')
+})
+app.post('/api/register', (req,res) => {
+    console.log(req.body)
+    res.send('Hello world')
+})
+
+app.listen(5001, () => {
+    console.log('Server started on 5001');
+})
